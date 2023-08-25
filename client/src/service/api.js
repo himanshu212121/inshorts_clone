457a8acd5515d05a975d28138084f39bb2780c08
himanshu212121 <2021pietcshimanshu079@poornima.org>
2023-08-25 13:44:29 +0530

    first commit

--- client/src/service/api.js
+++ client/src/service/api.js
@@ -1,7 +1,7 @@
 import axios from 'axios'
 
 export const getNews=async()=>{
-    const url='http://localhost:8000';
+    const url='inshorts-clone-one.vercel.app';
     try{
       return await axios.get(`${url}/news`)
     }catch(error){
